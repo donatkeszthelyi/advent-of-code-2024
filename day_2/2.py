11dfa296a7d14e8dda8420c5a532c99c340ff77b
donatkeszthelyi <keszthelyi.donat@gmail.com>
2024-12-02 12:53:20 +0100

    day 2

--- day_2/2.py
+++ day_2/2.py
@@ -7,25 +7,17 @@ df = pd.read_csv('2_data.txt', sep='\s+', header=None)
 
 #Defining the logic for the adjacent difference check
 def adj_diff_check(row):
-    
     row = row.dropna() #Drop NaN values
-  
-    #Check if any adjacent pair has a difference between 1 and 3 (inclusive)
-    for i in range(len(row) - 1):
+    for i in range(len(row) - 1): #Check if any adjacent pair has a difference between 1 and 3 (inclusive)
         if not (1 <= abs(row.iloc[i] - row.iloc[i + 1]) <= 3):
             return False
     return True
 
 #Defining the logic for the strict monotonity check
 def strict_monotonity_check(row):
-
     row = row.dropna() #Drop NaN values
-
-    #Check if strictly increasing
-    increasing = list(row) == sorted(row)
-    #Check if strictly decreasing
-    decreasing = list(row) == sorted(row, reverse=True)
-    
+    increasing = list(row) == sorted(row) #Check if strictly increasing
+    decreasing = list(row) == sorted(row, reverse=True) #Check if strictly decreasing
     return increasing or decreasing
 
 #Filtering through the dataframe, checking if a row is strictly increasing or strictly decreasing, and has an adjacent pair with a difference between 1 and 3
@@ -39,16 +31,11 @@ print(f"Safe count 1 = {safe_count_1}")
 
 #Defining the logic for the checks after value dropping
 def check_with_one_dropped(row):
-    
     row = row.dropna() #Drop NaN values
-
     for i in range(len(row)):
         row_without_i = row.drop(index=row.index[i]) #Drop the i-th value
-
-        #Check if the row without the i-th value satisfies both conditions
-        if strict_monotonity_check(row_without_i) and adj_diff_check(row_without_i):
+        if strict_monotonity_check(row_without_i) and adj_diff_check(row_without_i): #Check if the row without the i-th value satisfies both conditions
             return True
-        
     return False
 
 #Filtering through the dataframe, checking every row with the check_with_one_dropped function
